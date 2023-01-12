77de623c9e1f75aa11c1c007830d6fffc7e47096
pentonzxc <nikolai.urusov@gmail.com>
2023-01-12 18:05:29 +0300

    init

---
+++ src/main/java/com/nikolai/softarex/SoftarexTaskApplication.java
@@ -0,0 +1,13 @@
+package com.nikolai.softarex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftarexTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftarexTaskApplication.class, args);
+    }
+
+}
