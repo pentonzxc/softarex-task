32b92cfccfbbe4cc492d0401723612548f0e1a6b
pentonzxc <nikolai.urusov@gmail.com>
2023-02-03 00:19:45 +0300

    add listeners on fields count update

--- src/main/java/com/nikolai/softarex/App.java
+++ src/main/java/com/nikolai/softarex/App.java
@@ -2,11 +2,13 @@ package com.nikolai.softarex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
+@EnableAsync
 public class App {
 
     public static void main(String[] args) {
